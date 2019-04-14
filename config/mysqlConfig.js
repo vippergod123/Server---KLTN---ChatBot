eb2c09ca19fe3d83c97ba3a2ed2b2f90a6ef4d63
Cris <duytruong0702@gmail.com>
2019-04-14 17:04:19 +0700

    fix

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -12,8 +12,8 @@ const mysql = require('mysql');
 
 var sql = mysql.createPool({
   sockePath     : '/cloudsql/esoteric-code-237607:asia-east1:mysql-college-handbook',
-  // host: "35.194.253.115",
-  // port: 3306,
+  host: "35.194.253.115",
+  port:3306,
   user: "root",
   password: "",
   database: "college_handbook",  
