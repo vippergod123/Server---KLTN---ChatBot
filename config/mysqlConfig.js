91ac9019aa331699576afca3c00d68d29d11e969
Cris <duytruong0702@gmail.com>
2019-04-15 16:26:57 +0700

    fix bug

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -10,27 +10,36 @@ const mysql = require('mysql');
 // });
 
 
-var sql = mysql.createPool({
-  sockePath     : '/cloudsql/esoteric-code-237607:asia-east1:mysql-college-handbook',
+var sql = mysql.createConnection({
+  
   host: "35.194.253.115",
-  port:3306,
-  user: "root",
-  password: "",
+  // port:3306,
+  user: "admin",
+  password: "123",
   database: "college_handbook",  
-  connectionLimit:10000,
-  multipleStatements: true,
+  // socketPath  : '/cloudsql/esoteric-code-237607:asia-east1:mysql-college-handbook',
+  // connectionLimit:10000,
+  // multipleStatements: true,
 });
 
 
+sql.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  // console.log('Connected as thread id: ' + connection.threadId);
+});
 
-sql.getConnection((err, connection) => {
-    if(err) {
-        if(connection) connection.release();
-        console.log(err);
+
+// sql.getConnection((err, connection) => {
+//     if(err) {
+//         if(connection) connection.release();
+//         console.log(err);
         
-    } else  {
-    }
-})
+//     } else  {
+//     }
+// })
 
 
 
